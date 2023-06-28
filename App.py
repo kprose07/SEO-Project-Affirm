b0f6f760999ca06621b7c338de521d608cca0690
Khaylah <khaylahrscholar@gmail.com>
2023-06-28 17:57:16 -0400

    API Request

--- App.py
+++ App.py
@@ -1,2 +1,8 @@
-import requests
+from pip._vendor import requests
 url = "https://affirmations.dev/"
+r = requests.get(url)
+print(r.status_code)
+#print(r.json())
+print(r[i].get('affirmation'))
+
+add_Affirm = r[i].get('affirmation')
