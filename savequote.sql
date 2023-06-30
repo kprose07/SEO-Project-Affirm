0864362a8bcfc1b266b63cf5fecb9025a769287a
Paul Davis <pdavis1473@gmail.com>
2023-06-30 13:53:56 +0000

    File organization

--- savequote.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE myDataQuotes ( 
-    id INT(8) UNSIGNED NOT NULL auto_increment,
-    Quote VARCHAR(255) default NULL,
-    Author VARCHAR(255) default NULL,
-    Category VARCHAR(255) default NULL,
-    PRIMARY KEY (id)
-) AUTO_INCREMENT=1;
--- table.sql
+++
@@ -1,7 +0,0 @@
-create table myquotesdata(
-    qid int auto_increment,
-    Quote varchar(1000) null,
-    Author varchar(1000) null,
-    Category varchar(1000) null,
-    primary key(qid)
-);
